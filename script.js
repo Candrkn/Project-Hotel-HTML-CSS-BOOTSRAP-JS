6ad1e07f180f2118831d3f8288342bf46931e84b
Can Durukan <candrkn@gmail.com>
2024-10-07 22:33:27 +0300

    project ends

--- script.js
+++ script.js
@@ -80,3 +80,30 @@ window.addEventListener("scroll", () => {
         counter("count4", 50, 250, 3000)
     }
 })
+
+
+// ! Our Partner
+
+var ourPartnerSwiper = new Swiper(".our-partner", {
+    slidesPerView : 5,
+    spaceBetween : 30,
+    loop : true,
+    autoplay : {
+        delay : 2000,
+    },
+
+    breakpoints : {
+        '991': {
+            slidesPerView : 5,
+            spaceBetween : 10,
+        },
+        '767': {
+            slidesPerView : 3,
+            spaceBetween : 10,
+        },
+        '320': {
+            slidesPerView : 2,
+            spaceBetween : 10,
+        },
+    }
+})
